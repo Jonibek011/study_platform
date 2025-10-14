50cd4ad033c95d9895544165da6c20b2f0489572
Jonibek Orifjonov  Web Developer <147488141+Jonibek011@users.noreply.github.com>
2025-10-14 10:42:35 +0500

    test

--- src/pages/settings/Setting.jsx
+++ src/pages/settings/Setting.jsx
@@ -41,16 +41,18 @@ function Setting() {
     (ctx) => ctx.mobileMenu
   );
   return (
-    <section className="flex flex-col bg-second-bg w-full h-full">
+    <section
+      className={`flex flex-col bg-second-bg w-full h-full  ${
+        width > 640 && height < 600 && "overflow-y-auto h-[600px]"
+      }`}
+    >
       <Navbar />
       <div className="main-section flex">
         <div className="sidebar">
           <div
             className={`w-[70vw]  md:w-72 h-[calc(100vh-90px)] bg-main-bg px-5 py-6 absolute z-[999] top-[90px] md:top-0 ${
               isToggle ? "left-0" : "left-[-100%]"
-            } transition-all duration-200 md:static ${
-              width > 640 && height < 600 && "overflow-y-auto"
-            }`}
+            } transition-all duration-200 md:static`}
           >
             <span
               className="inline-block md:hidden absolute top-3 right-3"
